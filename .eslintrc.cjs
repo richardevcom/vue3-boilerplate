3aa29d988e0fce70866e79c3e2a758e18666b66d
richardev <hello@richardev.com>
2022-09-17 17:45:07 +0300

    ✅ Lint fix & updated packages

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,10 +2,13 @@
 require("@rushstack/eslint-patch/modern-module-resolution");
 
 module.exports = {
-  "root": true,
-  "extends": [
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
     "plugin:vue/vue3-essential",
     "eslint:recommended",
-    "@vue/eslint-config-prettier"
-  ]
-}
+    "@vue/eslint-config-prettier",
+  ],
+};
