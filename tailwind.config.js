73f9757021c61889748e7366d8b3f9acf063823f
richardevcom <hello@richardev.com>
2024-07-26 12:49:25 +0300

    ✨ Added upgraded version of Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+	content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
 }
-
