e87cc664220fa32425a40ea09aca0de96b2d8abd
richardevcom <hello@richardev.com>
2024-07-26 12:48:10 +0300

    ✨ Added upgraded version of Tailwind

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-    ...(process.env.NODE_ENV === "production" ? { cssnano: {} } : {}),
-  },
-};
+export default {
+	plugins: {
+		tailwindcss: {},
+		autoprefixer: {},
+		...(process.env.NODE_ENV === "production" ? { cssnano: {} } : {}),
+	},
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+export default {
+  content: [],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
+
